684541c5357029b2784a24ad7f8f645c5f920ff7
Mads Lundsby Bisgaard <mads@malbi.dk>
2024-07-30 16:13:41 +0200

    Add Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={cn("font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
